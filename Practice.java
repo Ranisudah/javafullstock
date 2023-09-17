cad78ffd1df6e3ad14b22e54b30b9020af70cf4e
Ranisudha <sudhakutty246@gmail.com>
2023-09-17 18:55:16 +0530

    first commit

---
+++ Practice.java
@@ -0,0 +1,32 @@
+
+
+import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        Scanner pra = new Scanner(System.in);
+        int studentname;
+        System.out.println("enter the studentname");
+        studentname = pra.nextInt();
+        switch (studentname) {
+            case 101: {
+                System.out.println("sugumaran");
+                break;
+            }
+            case 102: {
+                System.out.println("sudharani");
+                break;
+            }
+            case 103: {
+                System.out.println("siva");
+                break;
+            }
+            case 104: {
+                System.out.println("Bala");
+                break;
+            }
+
+        }
+    }
+}
